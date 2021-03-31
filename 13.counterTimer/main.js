af813e5b8a94b58e0ed65bd4bea9945b9885cec4
k@m@l <kabota000@gmail.com>
2021-03-31 16:43:10 +0800

    00=>>>>>>11

---
+++ 13.counterTimer/main.js
@@ -0,0 +1,85 @@
+const svg = document.querySelector('svg');
+
+const coundownEL = document.querySelector('.time-counter');
+
+const butt = document.querySelector(".start")
+const buttclear = document.querySelector(".pause")
+const buttreset = document.querySelector(".reset")
+
+console.log(buttreset)
+
+
+//counter 5 minutes
+
+const startingMinutes = 1
+let time = startingMinutes * 60
+
+
+
+function startTimer(){
+
+timer = setInterval(updateCountdown,1000);
+let time;
+}
+
+
+function pauseTimer(){
+    clearInterval(timer)
+}
+
+
+
+function resetTimer(){
+    location.reload()
+}
+
+butt.addEventListener('click',()=>{
+startTimer()
+butt.classList.add('hide')
+buttclear.classList.remove('hide')
+})
+
+
+buttclear.addEventListener('click', ()=>{
+    pauseTimer()
+    buttclear.classList.add('hide')
+    butt.classList.remove('hide')
+    
+
+})
+
+
+buttreset.addEventListener('click', ()=>{
+    resetTimer()
+    console.log('aa')
+    
+
+})
+
+function updateCountdown() {
+
+    const minutes = Math.floor(time / 60)
+    let seconds = time % 60; 
+
+    seconds = seconds < 10 ? `0${seconds}`:seconds
+
+    coundownEL.innerHTML = `${minutes}:${seconds}`
+
+    time--
+    
+    if (time < 0){
+        location.reload()
+    }
+}
+
+
+
+//update variable value of every hour sec and minutes
+
+currentTime = new Date();
+
+svg.style.setProperty('--start-seconds', currentTime.getSeconds());
+svg.style.setProperty('--start-minutes', currentTime.getMinutes());
+svg.style.setProperty('--start-hours', currentTime.getHours() % 12);
+
+
